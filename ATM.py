14dde46ad5562032ec3099696c06f660c43ee66c
Dev jaiswal <109809511+Devjaiswalntw@users.noreply.github.com>
2022-08-24 18:37:18 +0530

    Create ATM.py

---
+++ ATM.py
@@ -0,0 +1,17 @@
+# We have populated the solutions for the 10 easiest problems for your support.
+# Click on the SUBMIT button to make a submission to this problem.
+a,b=(int)input()
+if(a>b)
+{
+    print(b)
+}
+else{a
+    if(a%5!=0)
+    {
+        print(b)
+    }
+    else{
+        sum=(b-a)-0.50
+        print
+    }
+}
